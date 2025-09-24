1f930ad34d31f113516386f1c54728d76345d2ae
d11416163-jpg <d11416163@gs.takming.edu.tw>
2025-09-24 10:54:46 +0800

    備註

---
+++ 0924(2)/0924(2)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0924_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
